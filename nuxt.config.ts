1b31b6e2ff678b2161dc8522cfcf1ae72433f3a1
KarlBager <kallebager@gmail.com>
2024-09-13 14:09:01 +0200

    123

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  css: ['~/assets/main.css', '~/assets/global.css', '~/assets/flickity/dist/flickity.min.css', '@/assets/css/tailwind.css'],
+  css: ['~/assets/main.css', '~/assets/global.css', '~/assets/flickity/dist/flickity.min.css'],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
-  modules: [['@storyblok/nuxt', { accessToken: 'RbDFPddzRlJ3FzSDSKY9rgtt' }]],
-  buildModules: [
-    '@nuxtjs/tailwindcss',
-  ],
+  modules: [['@nuxtjs/tailwindcss'], ['@storyblok/nuxt', { accessToken: 'RbDFPddzRlJ3FzSDSKY9rgtt' }]],
   app: {
   head: {
     bodyAttrs: {
