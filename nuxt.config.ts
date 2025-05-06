4278e11096e453d03e01ef2e0c8a8518143b072a
KarlBager <kallebager@gmail.com>
2025-05-06 13:43:34 +0200

    123

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,7 +29,12 @@ export default defineNuxtConfig({
     meta: [{
       name: 'google-site-verification',
       content: 'EJqIOGhmaUOcCTX8CR2l_HbAc-n74lEg9kmg3BcM6w4'
-    }],
+    }, 
+    {
+      name: 'google-adsense-account',
+      content: 'ca-pub-6165622967405383'
+    }  
+  ],
     link: [{ rel: 'stylesheet', href: 'https://use.typekit.net/byd1adg.css' }, { rel: 'icon', href: 'https://www.media.karlbager.dk/media/favicon.png' }]
   }
 },
